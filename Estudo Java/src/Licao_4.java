86b8528350e9dae848059aa6b56f6b62358285fa
lpessolato <pessolatolucas@gmail.com>
2026-07-22 15:02:14 -0300

    Correção da lição 4

--- Estudo Java/src/Licao_4.java
+++ Estudo Java/src/Licao_4.java
@@ -4,26 +4,24 @@ public class Licao_4 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         boolean parar = false;
+
+        System.out.println("Digite um número para ser multiplicado");
+        int n = scanner.nextInt();
+        tabuada(n);
+
         do {
-            System.out.println("Digite a uma opção \n 1.Tabuada \n 2.Mostrar frase \n 3.Sair");
+            System.out.println("Opcão 1.Continuar \nOpcão 2.Sair");
             int opcao = scanner.nextInt();
             scanner.nextLine();
-            while (opcao<1 || opcao >3){
+            while (opcao<1 || opcao >2){
                 System.out.println("Digite uma opção válida.");
-                System.out.println("Digite a uma opção \n 1.Tabuada \n 2.Mostrar frase \n 3.Sair");
+                System.out.println("Opcão 1.Continuar \n Opcão 2.Sair");
                 opcao = scanner.nextInt();
             }
             if(opcao == 1){
-                System.out.println("Digite um número.");
-                int n = scanner.nextInt();
-                scanner.nextLine();
-                tabuada(n);
-            }
-            else if(opcao ==2){
                 System.out.println("Digite seu nome ou uma frase");
                 String nome = scanner.nextLine();
                 mostrarNome(nome);
-
             }
             else{
                 System.out.println("Obrigado por utilizar o nosso sistema, espero que tenha gostado.");
